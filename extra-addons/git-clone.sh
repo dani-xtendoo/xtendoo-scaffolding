02b6a4a23c3b1a9cd3d47f93b3ab1d80a292ef13
manuelcalerosolis <mcalero@gestool.es>
2019-12-20 19:57:03 +0100

    server-tools

--- extra-addons/git-clone.sh
+++ extra-addons/git-clone.sh
@@ -6,7 +6,6 @@ git clone --branch 12.0 --depth 1 https://github.com/Openworx/backend_theme.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/bank-payment.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/commission.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/community-data-files.git
-git clone --branch 12.0 --depth 1 https://github.com/hibou-io/hibou-odoo-suite.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/l10n-spain.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/mis-builder.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/purchase-workflow.git
@@ -18,5 +17,8 @@ git clone --branch 12.0 --depth 1 https://github.com/OCA/stock-logistics-barcode
 git clone --branch 12.0 --depth 1 https://github.com/OCA/web.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/stock-logistics-reporting.git
 git clone --branch 12.0 --depth 1 https://github.com/OCA/server-tools.git
+
+git clone --branch 12.0 --depth 1 https://github.com/hibou-io/hibou-odoo-suite.git
+
 git clone --branch master --depth 1 https://github.com/manuelcalerosolis/xtendoo.git
 
