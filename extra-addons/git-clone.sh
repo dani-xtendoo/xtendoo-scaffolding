3e1f38ec33df27b059b5776eaeb4830664cf318c
dani-ddl <dani--ef-@hotmail.com>
2020-02-04 12:44:57 +0100

    añadidos todos los forks

--- extra-addons/git-clone.sh
+++ extra-addons/git-clone.sh
@@ -1,27 +1,47 @@
-git clone --branch 12.0 --depth 1 https://github.com/OCA/account-financial-reporting.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/account-invoice-reporting.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/account-invoicing.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/account-payment.git
-git clone --branch 12.0 --depth 1 https://github.com/Openworx/backend_theme.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/bank-payment.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/commission.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/community-data-files.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/l10n-spain.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/mis-builder.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/purchase-workflow.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/reporting-engine.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/sale-reporting.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/sale-workflow.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/server-ux.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/stock-logistics-barcode.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/web.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/stock-logistics-reporting.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/server-tools.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/stock-logistics-workflow.git
-git clone --branch 12.0 --depth 1 https://github.com/OCA/stock-logistics-warehouse.git
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/account-financial-reporting.git
 
-git clone --branch 12.0 --depth 1 https://github.com/hibou-io/hibou-odoo-suite.git
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/account-invoice-reporting.git
 
-git clone --branch master --depth 1 https://github.com/manuelcalerosolis/xtendoo.git
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/account-invoicing.git
 
-git clone --branch master --depth 1 https://github.com/OCA/delivery-carrier.git
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/account-payment.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/backend_theme.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/bank-payment.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/commission.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/community-data-files.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/l10n-spain.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/mis-builder.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/purchase-workflow.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/reporting-engine.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/sale-reporting.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/sale-workflow.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/server-ux.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/stock-logistics-barcode.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/web.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/stock-logistics-reporting.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/server-tools.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/stock-logistics-workflow.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/stock-logistics-warehouse.git
+
+git clone --branch 12.0 --depth 1 https://github.com/dani-xtendoo/hibou-odoo-suite.git
+
+git clone --branch master --depth 1 https://github.com/dani-xtendoo/xtendoo.git
+
+git clone --branch master --depth 1 https://github.com/dani-xtendoo/delivery-carrier.git
